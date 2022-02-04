12c76cf1a55baa2aaa8212dd41b16fa2ec586166
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-02-04 14:45:59 +0300

    Add files via upload

---
+++ try-catch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                Console.WriteLine("Bir sayı giriniz");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı : " + a);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamalandı");
+            }
+            Console.WriteLine("------------------");
+            try
+            {
+                //int c = int.Parse(null);
+                //int c = int.Parse("test");
+                int c = int.Parse("-55000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+              Console.WriteLine("Boş değer girdiniz");
+              Console.WriteLine(ex);    
+            
+            }
+             catch (FormatException ex )
+            {
+              Console.WriteLine("Veri tipi uygun değil");
+              Console.WriteLine(ex);    
+            
+            }
+              catch (OverflowException ex )
+            {
+              Console.WriteLine("Çok küçük veya büyük değer girdiniz.");
+              Console.WriteLine(ex);    
+            
+            }
+        }
+    }
+}
