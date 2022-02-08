8c72d02b80bc456bf121aea6cd5d947e12d72fc9
Sumeyye Coskun <62007900+smyy96@users.noreply.github.com>
2022-02-08 22:50:58 +0300

    Update Program.cs

--- WhileForeach/Program.cs
+++ WhileForeach/Program.cs
@@ -6,7 +6,31 @@ namespace WhileForeach
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sayı giriniz :");
+            int a = int.Parse(Console.ReadLine());
+            int b = 1;
+            int c = 0;
+            while (b <= a)
+            {
+                c += b;
+                b++;
+            }
+            Console.WriteLine(c / a);
+            Console.WriteLine("--------------------------");
+            char e = 'a';
+            while (e < 'z')
+            {
+                Console.WriteLine(e);
+                e++;
+
+            }
+            Console.WriteLine("--------------------------");
+            string[] ogrenciler = { "Ali", "Ahmet", "Ayşe", "Aylin" };
+
+            foreach (var i in ogrenciler)
+            {
+               Console.WriteLine(i);
+            }
         }
     }
 }
