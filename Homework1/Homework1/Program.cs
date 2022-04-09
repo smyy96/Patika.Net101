fd04c5d2e2e3472802fc8dae2589b3b417ef7f41
smyy <smyy.cskn@hotmail.com>
2022-04-09 19:16:22 +0300

    Ödevin 2. sorusu eklendi.

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -30,12 +30,46 @@ namespace Homework1
 
 
 
+
+        /* 2.Ödev
+           Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+        */
+
+        public static void PositiveDivisor(int n, int m)
+        {
+            int[] num = new int[n];
+            Console.WriteLine($"{n} adet positif sayı giriniz.");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i + 1}. sayıyı giriniz: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine($"Girdiginiz sayılar arasındaki {m}'e bölünen sayılar.");
+            foreach (var item in num)
+            {
+                if (item % m == 0)
+                    Console.Write($"{item}, ");
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Ödev");
+            Console.WriteLine("1. Ödev");// Ödev 1
             Console.Write("Positif bir sayı giriniz: ");
             int postiveNum = int.Parse(Console.ReadLine());
             PositiveNumber(postiveNum);
+
+
+            Console.WriteLine("2. Ödev"); //Ödev 2
+            Console.Write("Positif bir sayı giriniz: ");
+            int size = int.Parse(Console.ReadLine());
+            Console.Write("Positif bir sayı giriniz: ");
+            int dividing = int.Parse(Console.ReadLine());
+            PositiveDivisor(size,dividing);
         }
     }
 }
