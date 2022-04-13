80b7ba7e8686dd06f09a7241118e17b48d75ba3c
smyy <smyy.cskn@hotmail.com>
2022-04-13 22:31:03 +0300

    3. soru eklendi.

---
+++ Homework2/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.*/
+
+            ArrayList ArrayVowel = new ArrayList();
+
+            string vowels= "aeıioöuü";
+
+            Console.Write("Cümle giriniz:");
+            string sentence = Console.ReadLine();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))// sesli harf var mı diye kontrol ediyor
+                {
+                    ArrayVowel.Add(sentence[i]); // diziye ekleme
+                }
+            }
+
+            ArrayVowel.Sort(); // sıralama
+            Console.WriteLine("---- Sıralaması ----");
+            foreach (var item in ArrayVowel) // ekrana yazdırma
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
