5d1b7b06ac22e6db6a6ef3e4347837058229ea24
smyy <smyy.cskn@hotmail.com>
2022-04-18 14:54:54 +0300

    Enum added.

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enum -> sayısal verilerı string ifadelerle saklamanızı sağlar. 
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün!");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.CokSicak)
+                System.Console.WriteLine("Hadi dışarıya çıkalım!");
+        }
+
+        enum Gunler
+        {
+            Pazartesi = 1,
+            Sali,
+            Carsamba,
+            Persembe,
+            Cuma = 23,
+            Cumartesi,
+            Pazar
+        }
+
+        enum HavaDurumu
+        {
+            Soguk = 5,
+            Normal = 20,
+            Sicak = 25,
+            CokSicak = 30
+        }
+    }
+}
